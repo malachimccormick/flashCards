c3bc4317155ddbf3e0e9d295091a1ee047a2976d
COR Students <corstudents@MacBook-Pro.localdomain>
2018-12-14 10:36:05 -0500

    added cards

--- flash_public/scripts/flashScripts.js
+++ flash_public/scripts/flashScripts.js
@@ -6,3 +6,20 @@ $('#back').click(function(){
     $('#cardAnswer').slideUp(-100)
     $('#cardQuestion').slideDown(250)
 })
+$('#addNew').click(function(){
+    $('.modal').show()
+    $('#addNew').hide()
+})
+$('#submit').click(function(){
+    $('#addNew').show()
+    $('.modal').hide()
+    $('#contentAdded').html('You added ' + ' cards')
+})
+$('#cancel').click(function(){
+    $('.modal').hide()
+    $('#addNew').show()
+})
+$('.hint').click(function(){
+    $('.hint').hide(-100)
+    $('.hintShow').show()
+})
