b92eccae67640d46c2eca7bf1d78a9ad876bc303
Malachi Mccormick <malachimccormick@Malachis-MacBook-Pro.local>
2018-12-29 21:31:18 -0500

    production ready

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const bodyParser=require('body-parser')//needed to receive post requests
-const MongoClient=require('mongodb').MongoClient
 const app=express()
+const MongoClient=require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectID
 
-let ObjectId=require('mongodb').ObjectID;
+
 let db;
 let flash;
 
@@ -12,53 +13,64 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/flash_public/'))
 
-MongoClient.connect('mongodb://Sandbox:Sandbox1@ds053156.mlab.com:53156/flashcards', (err, database) => {
+MongoClient.connect('mongodb://Sandbox:Sandbox1@ds053156.mlab.com:53156/flashcards',{ useNewUrlParser: true }, (err, database) => {
     error(err)
         db = database.db('flashcards')
             app.listen(3000, function(){
         console.log("listening on port 3000")
+        
     })
 })
-
+//reads the data in the database
 app.get('/',(req,res)=>{
     let curser= db.collection('flashcards').find().toArray(function(err,results){
         error(err)
             flash= results
         res.render('index')
     }) 
-    
-    })
-
-app.post('/addcard', (req,res)=>{
-    db.collection('flashcards').save(req.body,(err,result)=>{
-        error(err)
-        console.log('saved to database :)')
-    res.redirect('/')
-    })
 })
 
 app.get('/addcard', (req, res) => {
     res.send(flash)
+    
 })
-app.put('/editcard/:id',(req,res, next)=>{
-    let id = {
-        _id: ObjectID(req.params.id)
-    }
-    db.collection('flashcards').update({_id:id}),
-    error()
-    res.redirect('/')
-})
-app.delete('/addcard/:id', (req, res, next) => {
-    let id = {
-        _id: ObjectID(req.params.id)
-    }
-    db.collection('flashcards').update({
-            _id: id
-        }),
-        error()
+//creats new card
+app.post('/addcard', (req, res) => {
+    
+    db.collection('flashcards').insertOne(req.body, (err, result) => {
+        error(err)
         res.redirect('/')
+    })
+    //i messed up and had an id with a value of null. 
+    //I used .remove to erase it and left it here incase it is needed again
+    // db.collection('flashcards').remove({_id:null}, (err, result) => {
+    //     error(err)
+    //     console.log(req.body)
+    //     console.log('updated database :)' + result)
+    //     res.redirect('/')
+    // })
 })
 
-function error(err){
-    if(err) return console.log(err)
-}
+app.post('/update', (req, res) => {
+     let newCard = {
+        question: req.body.question,
+        hint: req.body.hint,
+        answer:req.body.answer
+        }
+    let id =req.body.id
+    db.collection('flashcards').updateOne({_id:ObjectId(id)},{$set:newCard}, (err, result) => {
+        error(err)
+            res.redirect('/')
+    })
+})
+app.post('/delete', (req, res) => {
+    let id =req.body.id
+        db.collection('flashcards').deleteOne({_id:ObjectId(id)}, (err, result) => {
+            error(err)
+                res.redirect('/')
+    })
+})            
+    function error(err){
+        if(err) return console.log(err)
+    }
+                       
